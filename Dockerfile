53fef666f663bd02241fda9a5544e298568a68a7
sng2nara <sng2nara@gmail.com>
2020-06-21 20:37:52 +0900

    change to nginx uwsgi

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM tiangolo/meinheld-gunicorn-flask:python3.8
+#FROM tiangolo/meinheld-gunicorn-flask:python3.8
+FROM tiangolo/uwsgi-nginx-flask:python3.8
 
 COPY ./app/requirements.txt /app/requirements.txt
 WORKDIR /app
--- app/main.py
+++ app/main.py
@@ -3,9 +3,6 @@ from flask import render_template, request, stream_with_context, Response
 
 from backlash import *
 
-import meinheld
-meinheld.set_max_content_length(2**10 * 2**10 * 2**10 * 1) # 1 GB
-
 app = Flask(__name__)
 # app.config['TEMPLATES_AUTO_RELOAD'] = True
 
